2d6954f34c6739ff536675a85f0ab69ea3c0c400
David Samueza <dsamueza@gmail.com>
2018-04-23 16:36:48 -0500

    Contexto

--- Realstate/Data/ApplicationDbContext.cs
+++ Realstate/Data/ApplicationDbContext.cs
@@ -5,38 +5,14 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Realstate.Models;
-using Realstate.Models.BaseDatos;
 
 namespace Realstate.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public virtual DbSet<Models.BaseDatos.Account> Account { get; set; }
-        public virtual DbSet<AspNetRoleClaims> AspNetRoleClaims { get; set; }
-        public virtual DbSet<AspNetRoles> AspNetRoles { get; set; }
-        public virtual DbSet<AspNetUserClaims> AspNetUserClaims { get; set; }
-        public virtual DbSet<AspNetUserLogins> AspNetUserLogins { get; set; }
-        public virtual DbSet<AspNetUserRoles> AspNetUserRoles { get; set; }
-        public virtual DbSet<AspNetUsers> AspNetUsers { get; set; }
-        public virtual DbSet<AspNetUserTokens> AspNetUserTokens { get; set; }
-        public virtual DbSet<Country> Country { get; set; }
-        public virtual DbSet<District> District { get; set; }
-        public virtual DbSet<Management> Management { get; set; }
-        public virtual DbSet<Parish> Parish { get; set; }
-        public virtual DbSet<Project> Project { get; set; }
-        public virtual DbSet<ProspectoAreas> ProspectoAreas { get; set; }
-        public virtual DbSet<Province> Province { get; set; }
-        public virtual DbSet<Region> Region { get; set; }
-        public virtual DbSet<Sector> Sector { get; set; }
-        public virtual DbSet<StatusTask> StatusTask { get; set; }
-        public virtual DbSet<Models.BaseDatos.Task> Task { get; set; }
-        public virtual DbSet<TypeProject> TypeProject { get; set; }
-        public virtual DbSet<TypeTask> TypeTask { get; set; }
-        public virtual DbSet<Zona> Zona { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
-
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
@@ -47,7 +23,7 @@ namespace Realstate.Data
             // Add your customizations after calling base.OnModelCreating(builder);
         }
 
-   
+
     }
 
 
